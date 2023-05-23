8105ac583b70b03110bd0d8d0294192d74390f06
Andy Gray <adgray@gmail.com>
2023-05-23 09:58:38 +0200

    updates

--- dist/index.js
+++ dist/index.js
@@ -27558,25 +27558,17 @@ async function run() {
                 " - past " +
                 daysToQuery +
                 " days";
-            
+
+            let isTLS = false;
             const transport = nodemailer.createTransport({
-                service: 'gmail',
+                host: smtpServer,
+                port: smtpServerPort,
+                secure: isTLS,
                 auth: {
                     user: authUser,
                     pass: authPwd,
                 },
             });
-
-            // let isTLS = false;
-            // const transport = nodemailer.createTransport({
-            //     host: smtpServer,
-            //     port: smtpServerPort,
-            //     secure: isTLS,
-            //     auth: {
-            //         user: authUser,
-            //         pass: authPwd,
-            //     },
-            // });
 
             const info = await transport.sendMail({
                 from: get_from(emailFrom, authUser),
